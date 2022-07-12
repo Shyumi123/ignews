9a7e94e8d95bfc32eae270c870fa38b685491b8a
Shyumi123 <sikor12@gmail.com>
2022-07-12 16:35:58 -0300

    configure: stripe ( do not duplicate user )

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 
@@ -5,10 +6,12 @@ import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
+    
   );
 }
 
